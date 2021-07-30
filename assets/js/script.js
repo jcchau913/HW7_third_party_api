bc4b08acb9b89b6a6bc6890949ce6e9f52003e73
Jeff Chau <jchau@us.jvckenwood.com>
2021-07-29 18:51:59 -0700

    refactor.

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,20 +30,12 @@ $(document).ready(function () {
         }
         $("#hour" + x).append("<button class='btn saveBtn col-md-1'><i class='fas fa-save'></i></button></div>");           
     }
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
-
+    for (x=9; x<=17; x++) {
+        $("#hour" + x + " .description").val(localStorage.getItem("hour" + x));
+    }
     $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-
         localStorage.setItem(time, text);
     })
 
